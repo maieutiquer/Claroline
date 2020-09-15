bdb8425f3b14df2e23d019d3c0b673c0b12ffa4b
Daniel Rodriguez <maieutiquer@gmail.com>
2020-09-15 20:06:25 +0300

    do not sed

--- .docker/web/entrypoint.sh
+++ .docker/web/entrypoint.sh
@@ -77,7 +77,7 @@ chmod -R 777 var/cache files/config var/log var/sessions files public/uploads
 echo "Disabling SAML in files/config/bundles.ini"
 # LightSaml\SymfonyBridgeBundle\LightSamlSymfonyBridgeBundle = true
 # LightSaml\SpBundle\LightSamlSpBundle = true
-sed 's/LightSaml\SymfonyBridgeBundle\LightSamlSymfonyBridgeBundle = true/LightSaml\SymfonyBridgeBundle\LightSamlSymfonyBridgeBundle = false/' files/config/bundles.ini
-sed 's/LightSaml\SpBundle\LightSamlSpBundle = true/LightSaml\SpBundle\LightSamlSpBundle = false/' files/config/bundles.ini
+# sed 's/LightSaml\SymfonyBridgeBundle\LightSamlSymfonyBridgeBundle = true/LightSaml\SymfonyBridgeBundle\LightSamlSymfonyBridgeBundle = false/' files/config/bundles.ini
+# sed 's/LightSaml\SpBundle\LightSamlSpBundle = true/LightSaml\SpBundle\LightSamlSpBundle = false/' files/config/bundles.ini
 
 exec "$@"
